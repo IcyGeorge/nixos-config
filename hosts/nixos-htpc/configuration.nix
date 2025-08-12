69a70b58e4d8c68a2e5feaa436ba25ce09c272c5
icygeorge <georgenaiem1@gmail.com>
2025-08-12 14:36:16 +0300

    htpc
    Signed-off-by: icygeorge <georgenaiem1@gmail.com>

---
+++ hosts/nixos-htpc/configuration.nix
@@ -0,0 +1,56 @@
+{ config, ... }: {
+  imports = [
+    ../../modules/nvidia.nix
+    ../../modules/dev.nix
+    ../../modules/xserver.nix
+    ../../modules/kitty.nix
+    ../../modules/slack.nix
+    ../../modules/zoom-us.nix
+    ../../modules/hardware.nix
+    ../../modules/nix-ld.nix
+    ../../modules/audio.nix
+    ../../modules/bluetooth.nix
+    ../../modules/fonts.nix
+    ../../modules/fuzzel.nix
+    ../../modules/fish.nix
+    ../../modules/git.nix
+    ../../modules/gnome.nix
+    ../../modules/gtk.nix
+    ../../modules/home-manager.nix
+    ../../modules/lazygit.nix
+    ../../modules/helix.nix
+    ../../modules/mako.nix
+    ../../modules/nemo.nix
+    ../../modules/nix.nix
+    ../../modules/spicetify.nix
+    ../../modules/systemd-boot.nix
+    ../../modules/niri.nix
+    ../../modules/users.nix
+    ../../modules/utils.nix
+    ../../modules/waybar.nix
+    ../../modules/xdg.nix
+    ../../modules/firefox.nix
+    ../../modules/clipse.nix
+    ../../modules/starship.nix
+
+    # You should let those lines as is
+    ./hardware-configuration.nix
+    ./variables.nix
+  ];
+
+  home-manager.users."${config.var.username}" = {
+    home = {
+      inherit (config.var) username;
+      homeDirectory = "/home/" + config.var.username;
+
+      # Don't touch this
+      stateVersion = "25.05";
+    };
+    programs.home-manager.enable = true;
+  };
+
+
+
+  # Don't touch this
+  system.stateVersion = "25.05";
+}
---
+++ hosts/nixos-htpc/variables.nix
@@ -0,0 +1,44 @@
+{ config
+, lib
+, ...
+}: {
+  imports = [
+    # Choose your theme here:
+    #../../themes/catppuccin.nix
+  ];
+
+  config.var = {
+    hostname = "nixos-htpc";
+    username = "gmelika";
+    configDirectory =
+      "/home/"
+      + config.var.username
+      + "/nixos-config"; # The path of the nixos configuration directory
+
+    keyboardLayout = "us";
+
+    location = "Cairo";
+    timeZone = "Africa/Cairo";
+    defaultLocale = "en_US.UTF-8";
+
+    git = {
+      username = "icygeorge";
+      email = "georgenaiem1@gmail.com";
+    };
+
+    autoUpgrade = false;
+    autoGarbageCollector = true;
+
+    features = {
+      androidDev = true; # android
+    };
+  };
+
+  # Let this here
+  options = {
+    var = lib.mkOption {
+      type = lib.types.attrs;
+      default = { };
+    };
+  };
+}
