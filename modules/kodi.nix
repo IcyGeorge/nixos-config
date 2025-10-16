c017dc2bb8e8c698142e4fba8f097eb3b5edc1b2
icygeorge <georgenaiem1@gmail.com>
2025-10-16 15:47:06 +0300

    kodi fix
    Signed-off-by: icygeorge <georgenaiem1@gmail.com>

--- modules/kodi.nix
+++ modules/kodi.nix
@@ -1,27 +1,27 @@
 { config, pkgs, ... }: {
 
-  # home-manager.users.${config.var.username} = {
-  #   programs.kodi = {
-  #     enable = true;
-  #     package = pkgs.kodi.withPackages (k-pkgs: [
-  #       k-pkgs.joystick
-  #       k-pkgs.pvr-iptvsimple
-  #     ]);
-  #   };
-  # };
+  home-manager.users.${config.var.username} = {
+    programs.kodi = {
+      enable = true;
+      package = pkgs.kodi.withPackages (k-pkgs: [
+        k-pkgs.joystick
+        k-pkgs.pvr-iptvsimple
+      ]);
+    };
+  };
 
-  # networking.firewall.enable = true;
-  # networking.firewall = {
-  #   allowedTCPPorts = [ 8081 ];
-  #   allowedUDPPorts = [ 8081 ];
-  #   allowedTCPPortRanges = [
-  #     { from = 27031; to = 27037; }
-  #   ];
-  #   allowedUDPPortRanges = [
-  #     { from = 27031; to = 27037; }
-  #   ];
-  # };
+  networking.firewall.enable = true;
+  networking.firewall = {
+    allowedTCPPorts = [ 8081 ];
+    allowedUDPPorts = [ 8081 ];
+    allowedTCPPortRanges = [
+      { from = 27031; to = 27037; }
+    ];
+    allowedUDPPortRanges = [
+      { from = 27031; to = 27037; }
+    ];
+  };
 
-  # users.users.${config.var.username}.extraGroups = [ "video" ];
+  users.users.${config.var.username}.extraGroups = [ "video" ];
 
 }
