9db751e80c0fbc9edfcf30901ad23edb1cf36088
icygeorge <georgenaiem1@gmail.com>
2026-01-28 17:47:44 +0200

    qutebrowser cleanup
    Signed-off-by: icygeorge <georgenaiem1@gmail.com>

--- dots/qutebrowser/config.py
+++ dots/qutebrowser/config.py
@@ -6,29 +6,29 @@ config.load_autoconfig(False)
 # c.url.default_page = "/home/x_user/.config/qutebrowser/custom_start_page/index.html"
 
 # Fonts
-c.fonts.default_family   = '8.5pt "JetBrainsMono Nerd Font"'
-c.fonts.completion.entry = '8.5pt "JetBrainsMono Nerd Font"'
-c.fonts.debug_console    = '8.5pt "JetBrainsMono Nerd Font"'
-c.fonts.prompts          = '8.5pt "JetBrainsMono Nerd Font"'
-c.fonts.statusbar        = '8.5pt "JetBrainsMono Nerd Font"'
+c.fonts.hints = '12pt "Maple Mono NF"'
+c.fonts.keyhint = '12pt "Maple Mono NF"'
+c.fonts.prompts = '12pt "Maple Mono NF"'
+c.fonts.downloads = '12pt "Maple Mono NF"'
+c.fonts.statusbar = '12pt "Maple Mono NF"'
+c.fonts.contextmenu = '12pt "Maple Mono NF"'
+c.fonts.messages.info = '12pt "Maple Mono NF"'
+c.fonts.debug_console = '12pt "Maple Mono NF"'
+c.fonts.completion.entry = '12pt "Maple Mono NF"'
+c.fonts.completion.category = '12pt "Maple Mono NF"'
+c.fonts.default_family   = '12pt "Maple Mono NF"'
 
 # Dark mode
 c.colors.webpage.preferred_color_scheme = "dark"
 c.colors.webpage.darkmode.policy.images = "smart"
 
 # BINDINGS
-config.bind(";t",       "open -t")                                                        # Open new tab
-config.bind(";m",       "hint links spawn mpv --keep-open=yes {hint-url}")                # Play video via mpv
-config.bind(";d",       "hint links spawn yt-dlp {hint-url} -P ~/downloads/qutebrowser")  # Download video from youtube using yt-dlp
-config.bind("<Ctrl+k>", "completion-item-focus prev", mode="command")
-config.bind("<Ctrl+j>", "completion-item-focus next", mode="command")
+# config.bind(";t",       "open -t")                                                        # Open new tab
 
 
 # WEBSITES PERMISSIONS
 ## Javascript
-# config.set("content.javascript.clipboard", "access", "https://chat.openai.com/*") # In order to be able to copy answers.
 config.set("content.javascript.clipboard", "access", "https://github.com/*") # In order to be able to copy code...etc
-# config.set("content.javascript.clipboard", "access", "https://discord.com/channels/@me/*") # In order to be able to copy code...etc
 
 # Audio capture & desktop capture
 config.set("content.desktop_capture", True, "https://discord.com/*")
@@ -37,15 +37,7 @@ config.set("content.media.audio_capture", True, "https://discord.com/*")
 # UI
 c.scrolling.smooth = True    # Smooth scrolling for making eyes feeling better
 c.statusbar.show   = "never" # Never show statusbar
-c.tabs.show        = "never" # Never show tabs
-c.scrolling.bar    = "never"
-c.zoom.default     = "95%"
-config.source("themes/default/base16-gruvbox-material-dark-medium.config.py") # Set the theme.
+config.source("@configDirectory@/dots/qutebrowser/themes/default/base16-gruvbox-material-dark-medium.config.py") # Set the theme.
 
 ## Other settings
 # config.set("downloads.location.directory", "~/downloads") # Change download location.
-
-
-# Check that if it works
-# config.set("content.notifications.presenter", "libnotify")
-# config.set("content.notifications.enabled", True, "https://x.com/*")
--- modules/qutebrowser.nix
+++ modules/qutebrowser.nix
@@ -1,9 +1,15 @@
 { config, pkgs, ... }:
+let
+  configDirectory = config.var.configDirectory;
+in
 {
   home-manager.users.${config.var.username} = {
     programs.qutebrowser = {
       enable = true;
-      extraConfig = builtins.readFile ../dots/qutebrowser/config.py;
+      extraConfig = builtins.readFile (pkgs.replaceVars ../dots/qutebrowser/config.py {
+        configDirectory = configDirectory;
+      });
+
     };
   };
 }
